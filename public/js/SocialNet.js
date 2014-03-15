5a5f2c7de133314fc080edc67b493c898faca6ef
David Ziegler <dzieglersf@gmail.com>
2014-03-15 14:06:03 -0700

    inital commit

---
+++ public/js/SocialNet.js
@@ -0,0 +1,30 @@
+define(['router'], function(router) {
+    var initialize = function() {
+        checkLogin(runApplication);
+    }
+
+    var checkLogin = function(callback) {
+        $.ajax("/account/authenticated", {
+            method: "GET",
+            success: function() {
+                return callback(true);    
+            },
+            error: function() {
+                return callback(false);
+            }
+        });
+    };
+
+    var runApplication = function(authenticated) {
+        if (!authenticated) {
+            this.window.location = 'login';
+        } else {
+            this.window.location = 'index';
+        };
+        Backbone.history.start();
+    };
+
+    return {
+        initialize: initialize
+    };
+});
