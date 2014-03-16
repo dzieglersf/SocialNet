b394f0b4b657a0543abe297b9a00b3a3e9ae9aad
David Ziegler <dzieglersf@gmail.com>
2014-03-16 10:58:25 -0700

    Part II, Chapter 6, Putting it all Together

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+var MemoryStore = require('connect').session.MemoryStore;
+var mongoose = require('mongoose');
+var config = {
+    mail: require('./config/mail')
+};
+
+var Account = require('./models/Account')(config, mongoose, nodemailer);
 
 app.configure(function(){
     app.set('view engine', 'jade');
     app.use(express.static(__dirname + '/public'));
+    app.use(express.limit('1mb'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session(
+        {secret: "SocialNet secret key", store: new MemoryStore()}));
+    mongoose.connect('mongodb://localhost/nodebackbone');
 });
 
 app.get('/', function(req, res) {
@@ -51,6 +65,39 @@ app.post('/login', function(req, res) {
         console.log('login was successful');
         res.send(200);
     });
+});
+
+app.post('/forgotpassword', function(req, res) {
+    var hostname = req.headers.host;
+    var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
+    var email = req.param('email', null);
+    if (null == email || email.length < 1) {
+        res.send(400);
+        return;
+    }
+
+    Account.forgotpassword(email, resetPasswordUrl, function(success) {
+        if (success) {
+            res.send(200);
+        } else {
+            // Username or password not found
+            res.send(404);
+        }
+    });
+});
+
+app.get('/resetPassword', function(req, res) {
+    var accountId = req.param('account', null);
+    res.render('resetPassword.jade', {locals: {accountId: accountId}});
+});
+
+app.post('/resetPassword', function(req, res) {
+    var accountId = req.param('accountId', null);
+    var password = req.param('password', null);
+    if (null != accountId && null != password) {
+        Account.changePassword(accountId, password);
+    }
+    res.render('resetPasswordSuccess.jade')
 });
 
 app.listen(8000);
---
+++ config/mail.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+    var mail = {    
+        service: "Gmail",
+        auth: {
+            user: "dzieglersf@gmail.com",
+            pass: "zizek41$"
+        }
+    };
+
+    return { mail: mail}
+}
