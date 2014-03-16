79829111eea5a1ca5be9151fa356e680af4b3b15
David Ziegler <dzieglersf@gmail.com>
2014-03-15 23:13:59 -0700

    added login

--- app.js
+++ app.js
@@ -18,5 +18,40 @@ app.get('/account/authenticated', function(req, res) {
     }
 });
 
+app.post('/register', function(req, res) {
+    var firstName = req.param('firstName', '');
+    var lastName = req.param('lastName', '');
+    var email = req.param('email', null);
+    var password = req. param('password', null);
+
+    if (null == email || numm == password) {
+        res.send(400);
+        return;
+    }
+    Account.register(email, password, firstName, lastName);
+    res.send(200);
+})
+
+app.post('/login', function(req, res) {
+    console.log('login request');
+    var email = req.param('email', null);
+    var password = req.param('password', null);
+
+    if (null == email || email.length < 1
+        || null == password || password.length < 1) {
+        res.send(400);
+        return;
+    } 
+
+    Account.login(email, password, function(success) {
+        if (!success) {
+            res.send(401);
+            return;
+        }
+        console.log('login was successful');
+        res.send(200);
+    });
+});
+
 app.listen(8000);
 console.log('listening on port 8000');
